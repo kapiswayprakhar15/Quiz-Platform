eb26fd8a14a71729c941ba4f013de78181947bcd
Manoj-Katta <manojkatta1173@gmail.com>
2023-12-10 11:58:32 +0530

    modified frontend and backend

--- server/server.js
+++ server/server.js
@@ -1,19 +1,36 @@
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 import { MongoClient } from 'mongodb';
+import * as dotenv from 'dotenv';
+import path from 'path';
 
-const app = express();
-const port = 3001;
+import connectDB from './config/connection.js';
+import { typeDefs, resolvers } from './schemas/index.js';
+import { authMiddleware } from './utils/auth.js';
 
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
 const mongoUrl = 'mongodb+srv://2021ucs0106:9x6hM0egCSi4t5Qf@cluster0.hm3fpk3.mongodb.net/Roadmaps?retryWrites=true&w=majority';
 
-async function startServer() {
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(new URL('../client/dist', import.meta.url).pathname)));
+}
+
+const startServer = async () => {
   try {
     const client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('Connected to MongoDB');
 
     const db = client.db('Roadmaps');
     const collection = db.collection('Javascript');
-    
+
+    // REST API
     app.get('/get-data', async (req, res) => {
       try {
         const data = await collection.find({}).toArray();
@@ -24,17 +41,27 @@ async function startServer() {
       }
     });
 
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      context: authMiddleware,
+    });
+
+    await server.start();
+    server.applyMiddleware({ app });
+
     process.on('SIGINT', () => {
       client.close();
       process.exit();
     });
 
-    app.listen(port, () => {
-      console.log(`Server is running on http://localhost:${port}`);
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     });
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
   }
-}
+};
 
 startServer();
